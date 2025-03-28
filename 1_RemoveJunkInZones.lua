dfb87abe14e81945e58fef15f8d50d04e04acd37
Christopher Neal <55216380+chrisneal72@users.noreply.github.com>
2025-03-28 10:31:45 -0700

    Update 1_RemoveJunkInZones.lua
    Wrapped in a function to make easier to put into a script file.

--- 1_RemoveJunkInZones.lua
+++ 1_RemoveJunkInZones.lua
@@ -1,12 +1,16 @@
-for currentZoneNum in pairs(TriggerZoneList) do
-    local sphere = trigger.misc.getZone(TriggerZoneList[currentZoneNum])
-    sphere.point.y = land.getHeight({x = sphere.point.x, y = sphere.point.z})
-    local volS = {
-        id = world.VolumeType.SPHERE,
-        params = {
-            point = sphere.point,
-            radius = sphere.radius
+function CleanUpMap (){
+    for currentZoneNum in pairs(TriggerZoneList) do
+        local sphere = trigger.misc.getZone(TriggerZoneList[currentZoneNum])
+        sphere.point.y = land.getHeight({x = sphere.point.x, y = sphere.point.z})
+        local volS = {
+            id = world.VolumeType.SPHERE,
+            params = {
+                point = sphere.point,
+                radius = sphere.radius
+            }
         }
-    }
-    world.removeJunk(volS)
-end
+        world.removeJunk(volS)
+    end
+}
+
+CleanUpMap ()
